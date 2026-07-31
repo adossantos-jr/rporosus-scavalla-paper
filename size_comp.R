6f3ae1bad6e175cdf483b37fbaeef2040017c038
adossantos-jr <alexandre.ricardosantos@ufpe.br>
2026-07-31 16:02:43 -0300

    Size comp plots

--- size_comp.R
+++ size_comp.R
@@ -13,8 +13,19 @@ sbra_data = ldata %>% subset(species == "Scomberomorus brasiliensis")
 
 ggplot(rpor_data)+
   geom_histogram(aes(y = fl, fill = state))+
-  facet_wrap(~year)
+  scale_fill_manual(values = c('turquoise3', 'goldenrod1'))+
+  facet_wrap(~year)+
+  labs(y = 'FL (cm)', fill = 'State')+
+  ggtitle('Rhizoprionodon porosus')+
+  theme(axis.title.x = element_blank(),
+        plot.title = element_text(face = 'italic'))
 
 ggplot(sbra_data)+
   geom_histogram(aes(y = fl, fill = state))+
-  facet_wrap(~year)
+  scale_fill_manual(values = c('turquoise3', 'goldenrod1'))+
+  facet_wrap(~year)+
+  labs(y = 'FL (cm)', fill = 'State')+
+  ggtitle('Scomberomorus brasiliensis')+
+  theme(axis.title.x = element_blank())+
+  theme(axis.title.x = element_blank(),
+        plot.title = element_text(face = 'italic'))
